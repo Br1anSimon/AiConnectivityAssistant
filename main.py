580e2107153a80bd77bf8346bf59a32de66b7b83
DiamondShadows <bsimon2005@gmail.com>
2023-08-13 20:25:10 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from elevenlabs import set_api_key
+from elevenlabs import voices, generate, play
+import os
+import openai
+openai.api_key = ("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")
+
+set_api_key("98cfe093485bf42de7faecbd361eaaf2")
+
+voices = voices()
+
+messages =[{"role": "system", "content": "Your name is Master Chief, also known as John-117."},
+           {"role": "user", "content": "Your name is Master Chief, also known as John-117."}]
+while True:
+    message = input("User : ")
+    if message:
+        messages.append(
+            {"role": "user", "content": message},
+        )
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+    reply = chat.choices[0].message.content
+    #print(f"ChatGPT: {reply}")
+    messages.append({"role": "assistant", "content": reply})
+
+    audio = generate(
+        text=reply,
+        voice="oaCr9QCE9XvkD117OnVy",
+        model="eleven_multilingual_v1"
+    )
+
+    print(f'Master Chief: {reply}')
+    play(audio)
