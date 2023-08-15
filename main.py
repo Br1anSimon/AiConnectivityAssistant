2b2aa7f9b42de6d1fc20d04f1ed8d61c2e55444d
DiamondShadows <bsimon2005@gmail.com>
2023-08-14 22:55:58 -0400

    ffmpeg not working

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 from elevenlabs import set_api_key
 from elevenlabs import voices, generate, play
-import os
 import openai
-openai.api_key = ("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")
+from whisper_mic.whisper_mic import WhisperMic
+from numba.core.errors import NumbaDeprecationWarning, NumbaPendingDeprecationWarning
+import warnings
+
+openai.api_key = ("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")
 
 set_api_key("98cfe093485bf42de7faecbd361eaaf2")
 
 voices = voices()
 
+mic = WhisperMic()
+warnings.simplefilter('ignore', category=NumbaDeprecationWarning)
+warnings.simplefilter('ignore', category=NumbaPendingDeprecationWarning)
+
+
 messages =[{"role": "system", "content": "Your name is Master Chief, also known as John-117."}]
+chat_completion = ""
+talking = mic.listen()
+
 while True:
-    message = input("User : ")
+
+    message = input("User: " + talking)
     if message:
         messages.append(
             {"role": "user", "content": message},
         )
-        chat = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages
+        chat_completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
         )
-    reply = chat.choices[0].message.content
-    #print(f"ChatGPT: {reply}")
+    reply = chat_completion.choices[0].message.content
+    print(f"ChatGPT: {reply}")
     messages.append({"role": "assistant", "content": reply})
 
     audio = generate(
